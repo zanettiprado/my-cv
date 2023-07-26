7e2919f6f1afc56a0bc393e761beacdbe7e7de53
zanettiprado <zanettiprado@gmail.com>
2023-07-26 12:36:32 +0000

    add email section to receive our contacts

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,30 @@
+function sendMail(contactForm) {
+    emailjs.send("service_bpya8z7", "felipe", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+        .then(function (response) {
+            console.log('SUCCESS!', response);
+        }, function (error) {
+            console.log('FAILED...', error);
+        });
+    return false;
+};
+
+function sendMail(contactForm) {
+    emailjs.send("service_bpya8z7", "felipe", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
